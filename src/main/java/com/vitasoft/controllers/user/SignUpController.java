80a2fcdd01f3ea378471dbd21571ba8b7a768fa9
pochanin <roma_02.97@mail.ru>
2019-04-29 15:18:24 +0300

    Adding sign-up page

--- src/main/java/com/vitasoft/controllers/user/SignUpController.java
+++ src/main/java/com/vitasoft/controllers/user/SignUpController.java
@@ -54,7 +54,6 @@ public class SignUpController {
 			usersConnectionRepository.createConnectionRepository(connection.getKey().getProviderUserId()).addConnection(connection);
 		}
 		
-		return ""
 		return new ModelAndView("redirect:/");
 	}
 	
